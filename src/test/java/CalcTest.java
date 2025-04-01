7d00f6da092843092b30d0b191a135fef2abcc7c
Dmitriy Naumov <bear6818@gmail.com>
2025-04-01 11:56:10 +0300

    first com

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,7 @@
+public class CalcTest {
+    public static void main(String[] args) {
+        Calc calc = new Calc();
+        int result = calc.sum(1, 2);
+        System.out.println(result);
+    }
+}
