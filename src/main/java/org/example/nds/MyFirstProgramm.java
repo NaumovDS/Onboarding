588b17d23b80603106f663c511d395ca2b609b34
Dmitriy Naumov <bear6818@gmail.com>
2025-04-01 12:27:40 +0300

    пакет

---
+++ src/main/java/org/example/nds/MyFirstProgramm.java
@@ -0,0 +1,8 @@
+package org.example.nds;
+
+
+public class MyFirstProgramm {
+    public static void main() {
+        System.out.println("Hello and welcome!");
+        }
+    }
--- src/test/java/CalcTest.java
+++
@@ -1,7 +0,0 @@
-public class CalcTest {
-    public static void main(String[] args) {
-        Calc calc = new Calc();
-        int result = calc.sum(1, 2);
-        System.out.println(result);
-    }
-}
